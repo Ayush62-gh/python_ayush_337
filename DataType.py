ea818e19b4836c73b8f575a3bf2ac961b9ace9b6
Ayush14169 <a12219665@gmail.com>
2025-08-28 14:47:06 +0530

    file created

---
+++ DataType.py
@@ -0,0 +1,19 @@
+var1 = 20
+var2 = 20.5
+var3 = "Hello world"
+var4 = True
+print(type(var1),type(var2),type(var3),type(var4))
+len(var3)
+print(len(var3))
+print(var3[4])
+print("world" in var3)   #in is used to check if a substring is present in a string or not
+
+if 20>10:
+    print("20 is greater than 10")
+else:
+    print("10 is greater than 20")
+
+print(var3[-4:-2])
+
+print (var3.upper())
+print (var3.strip())
