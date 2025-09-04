9587672dc5aa9a0bfddf1a89fa0e55092b22f9a1
Ayush14169 <a12219665@gmail.com>
2025-09-04 14:51:53 +0530

    unit 1 is done here

---
+++ Lecture.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the first number:"))
+print(a)
+b=int(input("Enter the second number:"))
+print(b)
+
+#if (a>b):
+ #   print("a is greater than b")
+#elif (b>a):
+  #  print("b is greater than a")
+#else:
+ #   print("a is equal to b")
+print("a") if a>b else print("b")
