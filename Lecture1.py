910b1419be09cd37c31f303b74d78e59410b9f5a
Ayush14169 <aakarsh4555@gamil.com>
2025-09-09 12:03:14 +0530

    learned match statement

---
+++ Lecture1.py
@@ -0,0 +1,26 @@
+day = 5
+
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Invalid day number! Please enter 1–7.")
+
+
+
+#while loop
+i=1
+while i<9:
+    print(i)
