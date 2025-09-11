735935f715112ec145400b4a0ed9a1f4703395d8
Ayush14169 <aakarsh4555@gmail.com>
2025-09-11 14:42:56 +0530

    making commits

--- BreakContinue.py
+++ BreakContinue.py
@@ -4,3 +4,16 @@ while i<6:
     if i==3:
         continue
     print(i)
+
+fruits = ["apple", "banana","cherry"]
+for x in fruits:
+    print(x)
+    if x=="banana":
+        break
+
+#nested loop
+a=["A","B","C"]
+fruits = ["apple", "banana","cherry"]
+for x in a:
+    for y in fruits:
+        print(x,y)
