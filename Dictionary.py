a77cc05eef9f747787fdce747199571aa5b64e16
Ayush62-gh <ayushkumar.dev41@gmail.com>
2025-09-19 15:25:54 +0530

    printed ditionary with individual print statement

--- Dictionary.py
+++ Dictionary.py
@@ -3,4 +3,6 @@ student = {
     "roll_no": "2400320100337",
     "branch": "CSE"
 }
-print(student)
+print("student name " + student["name"])
+print("student roll number is " + student["roll_no"])
+print("student branch is "+student["branch"])
