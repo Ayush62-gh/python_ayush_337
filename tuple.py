bafefff9b35697c5ade86960493273690055bbf4
Ayush62-gh <ayushkumar.dev41@gmail.com>
2025-09-23 11:51:37 +0530

    tuple coverted to list for adding values

--- tuple.py
+++ tuple.py
@@ -1,3 +1,21 @@
-mytuple = ("apple","banana","cherry")
-print(mytuple)
-print(len(mytuple))
+tuple1 = ("apple","banana","cherry")
+print(tuple1)
+print(len(tuple1))
+tuple2 = (1,5,7,9,3)
+tuple3 = (True,False,False)
+
+#constructor
+T = tuple(("apple","banana","cherry","kiwi"))
+print(T)
+print(T[-2])
+#range of indexes
+print(T[1:3])
+if "apple" in T:
+    print("yes, 'apple' is in the tuple")
+#cannot change values in tuple
+#T[0]="kiwi" #gives error because tuples are immutable
+#but we can convert tuple to list and change it
+L =list(T)
+L[0]="kiwi"
+T=tuple(L)
+print(T)
